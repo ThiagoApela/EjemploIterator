794b2e205cf20e2a61b701d027cea2670c68645f
ThiagoApela <deibysantiago16@gmail.com>
2023-09-14 23:52:00 -0500

    repositorio EjemploIterator

---
+++ src/iterator/Notification.java
@@ -0,0 +1,19 @@
+package iterator;
+
+public class Notification {
+    String Notification;
+    
+    public Notification(
+        String notification) {
+            this.Notification = notification;
+        }
+
+    public String getNotification() {
+        return Notification;
+    }
+
+    public void setNotification(String notification) {
+        Notification = notification;
+    }
+        
+}
---
+++ src/iterator/NotificationCollection.java
@@ -0,0 +1,33 @@
+package iterator;
+
+public class NotificationCollection implements Collection{
+    static final int MAX_ITEMS = 6;
+    int numberOfItems = 0;
+    Notification[] notificationList;
+
+    public NotificationCollection() {
+        notificationList = new Notification[MAX_ITEMS];
+
+        addItem("Notification 1");
+        addItem("Notification 2");
+        addItem("Notification 3");
+        addItem("Notification 4");
+        addItem("Notification 5");
+        addItem("Notification 6");
+    }
+    public void addItem(String str){
+        Notification notification =
+        new Notification(str);
+        if (numberOfItems >= MAX_ITEMS){
+            System.err.println("Full");
+        }else{
+            notificationList[numberOfItems] = notification;
+            numberOfItems = numberOfItems + 1;
+        }
+    }
+    public Iterator createIterator(){
+        return new NotificationIterator(notificationList);
+    }
+
+
+}
